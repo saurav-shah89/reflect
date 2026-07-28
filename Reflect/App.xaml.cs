7f9cd449ec5ece0d4440277dcd0ddbc6d4c5312b
saurav-shah89 <snowflakenuisance@gmail.com>
2026-07-28 21:52:49 +0545

    chore: set up the MAUI Blazor project
    Empty MAUI Blazor Hybrid app targeting Android, iOS, MacCatalyst and Windows.
    Windows is set to build unpackaged so it runs without Developer Mode.

---
+++ Reflect/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Reflect;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "Reflect" };
+	}
+}
